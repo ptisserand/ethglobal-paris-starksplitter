2347fe46f662a1489192f18a4dce5a59df614ffb
Patrice Tisserand <p.tisserand@gmail.com>
2023-07-23 08:31:13 +0200

    app: add starknet dependencies

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import cryptography_flutter
+import mobile_scanner
+import path_provider_foundation
+import secure_store
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SecurestorePlugin.register(with: registry.registrar(forPlugin: "SecurestorePlugin"))
 }
