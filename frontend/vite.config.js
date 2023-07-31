45743eba4c9622971e697e19885262ae9229f7bd
Patrice Tisserand <p.tisserand@gmail.com>
2023-07-31 19:15:01 +0200

    frontend: use default vite output directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    build: {
-      outDir: 'build',
-    },
     // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
     define: {
         'process.env': {},
